39ef4dc4f7e9b400e4e9c3f5bbaa39a3075258a1
sarisiza <sarita.iza@gmail.com>
2024-08-04 12:19:55 -0500

    Fixed bug where adding buttons where disappearing when adding a lot of feelings or trackers

--- app/src/main/java/com/upakon/moonlog/ui/screens/ProfileScreen.kt
+++ app/src/main/java/com/upakon/moonlog/ui/screens/ProfileScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -25,6 +26,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.upakon.moonlog.R
 import com.upakon.moonlog.utils.UiState
@@ -72,10 +74,14 @@ fun ProfileScreen(
                 val moodsState = viewModel.feelingsList.collectAsState().value
                 Text(
                     text = stringResource(id = R.string.my_moods),
-                    style = MaterialTheme.typography.titleMedium
+                    style = MaterialTheme.typography.titleMedium,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(10.dp)
                 )
                 LazyColumn(
-                  modifier = Modifier.padding(10.dp)
+                  modifier = Modifier
+                      .padding(10.dp)
+                      .fillMaxHeight(0.8f)
                 ) {
                     when (moodsState){
                         is UiState.ERROR -> {
@@ -101,6 +107,7 @@ fun ProfileScreen(
                     onClick = { showMoodEditor = true },
                     modifier = Modifier
                         .fillMaxWidth()
+                        .fillMaxHeight()
                 ) {
                     Row {
                         Icon(
@@ -130,10 +137,14 @@ fun ProfileScreen(
                 val trackersState = viewModel.trackersList.collectAsState().value
                 Text(
                     text = stringResource(id = R.string.my_trackers),
-                    style = MaterialTheme.typography.titleMedium
+                    style = MaterialTheme.typography.titleMedium,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(10.dp)
                 )
                 LazyColumn(
-                    modifier = Modifier.padding(10.dp)
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .fillMaxHeight(0.8f)
                 ) {
                     when (trackersState) {
                         is UiState.ERROR -> TODO()
@@ -156,6 +167,7 @@ fun ProfileScreen(
                     onClick = { showTrackerEditor = true },
                     modifier = Modifier
                         .fillMaxWidth()
+                        .fillMaxHeight()
                 ) {
                     Row {
                         Icon(
